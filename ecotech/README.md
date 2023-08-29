81bb594924cbba11b61a5a858216025bb18eaf86
Lucas <117243622+Lucasf-Ramos@users.noreply.github.com>
2023-08-29 19:40:36 -0300

    Html e css do registrar cliente adicionados

---
+++ ecotech/README.md
@@ -0,0 +1,2 @@
+# tcc-sustentabilidade
+repositório do TCC 2023/2024 ITB Brasilio flores - tema: loja de compra e reciclagem de eletrônicos
---
+++ ecotech/ecotech2/src/test/java/ecotech/tcc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ecotech.tcc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
